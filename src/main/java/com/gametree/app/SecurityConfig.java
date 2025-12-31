08d8c11a6fb2405301ed7628722ede8cc1b2c1cd
eoizorak <iz0r4k@gmail.com>
2025-12-30 23:52:35 -0300

    Adicionando Spring Security com login

---
+++ src/main/java/com/gametree/app/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.gametree.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            
+            .csrf(csrf -> csrf.disable())
+            
+            
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin/**").authenticated() 
+                .anyRequest().permitAll()                     
+            )
+            
+
+            .formLogin(form -> form
+                .defaultSuccessUrl("/admin", true)
+                .permitAll()
+            )
+            
+
+            .logout(logout -> logout.logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+}
