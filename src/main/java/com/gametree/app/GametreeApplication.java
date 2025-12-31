c1f7f69e44250e69fb6ef05fd14c9a2ed1f381d5
eoizorak <iz0r4k@gmail.com>
2025-12-30 21:04:36 -0300

    Nascimento do Projeto GameTree

---
+++ src/main/java/com/gametree/app/GametreeApplication.java
@@ -0,0 +1,13 @@
+package com.gametree.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gametree/app/GametreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametree.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
